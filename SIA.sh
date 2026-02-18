32c9eaa610c2114f66d67a61700e5120c5f1fe59
Benjamin <jdonadelli@gmail.com>
2026-02-18 19:44:14 -0300

    statusinvest csv

--- SIA.sh
+++ SIA.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Inicie o ssh-agent
 eval "$(ssh-agent -s)"
 
 # Adicione a chave ao agente
-ssh-add ~/.ssh/id_rsa
+ssh-add /home/yair/.ssh/id_rsa
 
 # Navegue até o diretório do repositório
 cd /home/yair/GHub/TD/
@@ -10,13 +12,13 @@ cd /home/yair/GHub/TD/
 # Ative o ambiente Python pythorix
 source ~/bin/pythorix/bin/activate
 
-DISPLAY=:0 ~/bin/pythorix/bin/python TesouroDireto_Dia_v3.py
+DISPLAY=:0 /home/yair/bin/pythorix/bin/python3 SIA.py
 
 # Adicione alterações ao staging
 git add .
 
 # Faça o commit com uma mensagem
-git commit -m "Atualização automatica de tesouro_direto.json"
+git commit -m "Atualização automatica"
 
 # Faça o push para o repositório remoto
 git push origin main
