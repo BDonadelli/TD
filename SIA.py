29077c18d550c0a2161971d370d763ec5dbae427
Benjamin <jdonadelli@gmail.com>
2026-02-19 12:23:04 -0300

    latin

--- SIA.py
+++ SIA.py
@@ -1,57 +1,57 @@
-import os
-from time import sleep
-from datetime import date
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
-# Configurações
-data_path = os.path.join(os.getcwd(), "data")
-os.makedirs(data_path, exist_ok=True)
-
-# Configurar Chrome
-service = Service(ChromeDriverManager().install())
-opts = webdriver.ChromeOptions()
-opts.add_experimental_option("detach", True)
-opts.add_experimental_option("prefs", {
-    "download.default_directory": data_path,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-})
-
-
-
-# Abrir navegador
-driver = webdriver.Chrome(service=service, options=opts)
-driver.get('https://statusinvest.com.br/acoes/busca-avancada')
-
-# Definir XPaths
-path_busca = '//div/button[contains(@class,"find")]'
-path_download = '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a/span'
-
-# Executar busca
-# print('====== Busca')
-driver.find_element(By.XPATH, path_busca).click()
-sleep(3)
-
-# Download
-# print('====== Download')
-driver.find_element(By.XPATH, path_download).click()
-sleep(3)
-
-driver.quit()
-today = date.today().strftime('%d/%m/%Y')
-
-
-for filename in os.listdir(data_path):
-    if 'SI_Acoes' in filename:
-        os.remove(data_path+'/'+filename)
-os.rename(data_path+'/'+ 'statusinvest-busca-avancada.csv', data_path+'/'+'SI_Acoes.csv')
-
-### Fundamentus 
-
+# import os
+# from time import sleep
+# from datetime import date
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# 
+# # Configurações
+# data_path = os.path.join(os.getcwd(), "data")
+# os.makedirs(data_path, exist_ok=True)
+# 
+# # Configurar Chrome
+# service = Service(ChromeDriverManager().install())
+# opts = webdriver.ChromeOptions()
+# opts.add_experimental_option("detach", True)
+# opts.add_experimental_option("prefs", {
+#     "download.default_directory": data_path,
+#     "download.prompt_for_download": False,
+#     "download.directory_upgrade": True,
+#     "safebrowsing.enabled": True
+# })
+# 
+# 
+# 
+# # Abrir navegador
+# driver = webdriver.Chrome(service=service, options=opts)
+# driver.get('https://statusinvest.com.br/acoes/busca-avancada')
+# 
+# # Definir XPaths
+# path_busca = '//div/button[contains(@class,"find")]'
+# path_download = '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a/span'
+# 
+# # Executar busca
+# # print('====== Busca')
+# driver.find_element(By.XPATH, path_busca).click()
+# sleep(3)
+# 
+# # Download
+# # print('====== Download')
+# driver.find_element(By.XPATH, path_download).click()
+# sleep(3)
+# 
+# driver.quit()
+# today = date.today().strftime('%d/%m/%Y')
+# 
+# 
+# for filename in os.listdir(data_path):
+#     if 'SI_Acoes' in filename:
+#         os.remove(data_path+'/'+filename)
+# os.rename(data_path+'/'+ 'statusinvest-busca-avancada.csv', data_path+'/'+'SI_Acoes.csv')
+# 
+# ### Fundamentus 
+# 
 
 import requests
 import pandas as pd
@@ -65,4 +65,4 @@ header = {
 r1 = requests.get(url1, headers=header)
 dfs = pd.read_html(StringIO(r1.text), decimal=',', thousands='.')[0]
 
-dfs.to_csv("data/fundamentuspp.csv" , sep=';' )
+dfs.to_csv("data/fundamentuspp.csv", sep=';', encoding='latin-1', index=False)
